d9f1b2418a1847c868906ffb2cdb8c90ed47e733
antoniocs <antoniocs@gmail.com>
2015-09-30 09:29:54 +0100

    Added more tests

--- DataStructures/LinkedList/Single/Test/test.c
+++ DataStructures/LinkedList/Single/Test/test.c
@@ -5,25 +5,52 @@
 
 int tests_run = 0;
 int asserts_run = 0;
-static LinkedList *ll;
 
 static char *test_LLInit() {
-    ll = LLInit();
+    LinkedList *ll = LLInit();
 
     mu_assert("Unable to create Linked List", ll);
     return 0;
 }
 
-static char *test_LLAddBase() {    
+static char *test_LLAddTail() {
+    LinkedList *ll = LLInit();
+
     char data1[] = "teste";
     char data2[] = "teste";
     char data3[] = "teste";
     char data4[] = "teste";
 
-    LLAddBase(ll, data1);
-    LLAddBase(ll, data2);
-    LLAddBase(ll, data3);
-    LLAddBase(ll, data4);
+    LLAddTail(ll, data1);
+    LLAddTail(ll, data2);
+    LLAddTail(ll, data3);
+    LLAddTail(ll, data4);
+        
+    mu_assert("Incorrect assignment 1", ll->head->data == data1);
+    mu_assert("Incorrect assignment 2", ll->head->next->data == data2);
+    mu_assert("Incorrect assignment 3", ll->head->next->next->data == data3);
+    mu_assert("Incorrect assignment 4", ll->head->next->next->next->data == data4);
+
+    return 0;
+}
+
+static char *test_LLAddHead() {
+    LinkedList *ll = LLInit();
+    
+    char data1[] = "teste";
+    char data2[] = "teste";
+    char data3[] = "teste";
+    char data4[] = "teste";
+
+    LLAddHead(ll, data1);
+    LLAddHead(ll, data2);
+    LLAddHead(ll, data3);
+    LLAddHead(ll, data4);
+
+    mu_assert("Incorrect assignment 1", ll->head->data == data4);
+    mu_assert("Incorrect assignment 2", ll->head->next->data == data3);
+    mu_assert("Incorrect assignment 3", ll->head->next->next->data == data2);
+    mu_assert("Incorrect assignment 4", ll->head->next->next->next->data == data1);
 
     return 0;
 }
@@ -31,7 +58,8 @@ static char *test_LLAddBase() {
 
 mu_all_tests(
         test_LLInit,
-        test_LLAddBase
+        test_LLAddTail,
+        test_LLAddHead
 )
 
 mu_main()
